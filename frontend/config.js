4c239c3b90c5d4b144b1fcd5c39da16f6c83058c
vaniyarehan <vrehan33@gmail.com>
2025-12-09 18:00:48 +0500

    frontend without integration with the backend

---
+++ frontend/config.js
@@ -0,0 +1,7 @@
+// config.js
+
+// Using my laptop's Wi-Fi IPv4 Address (192.168.1.3) and the default FastAPI port (8000).
+export const API_BASE_URL = "http://192.168.1.3:8000";
+
+// For WebSocket connection
+export const WS_BASE_URL = "ws://192.168.1.3:8000";
